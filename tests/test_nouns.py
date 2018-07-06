a8be7f48d707d0c5c091a2d6464544cafcab9b07
David R. Mortensen <davidmortensen@gmail.com>
2018-07-06 18:32:49 -0400

    Many updates!

--- tests/test_nouns.py
+++ tests/test_nouns.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+#! -*- coding: utf-8 -*-
 
 from subprocess import Popen, PIPE
 import unittest
@@ -13,4 +14,19 @@ class NounTests(unittest.TestCase):
         return set(analyses.decode('utf-8').strip().split('\n'))
 
     def test_janataavata(self):
-        self.assertEqual(self._get_analyses('ජනතාවට'), {'ජනතාව+N+Dat+Sg+Anim'})
+        self.assertEqual(self._get_analyses('ජනතාවට'), {'ජනතාව+N+Dat+Sg'})
+
+    def test_minisaagee(self):
+        self.assertEqual(self._get_analyses('මිනිසාගේ'), {'මිනිසා+N+GenLoc+Sg+Anim'})
+
+    def test_sanggamyee(self):
+        self.assertEqual(self._get_analyses('සංගමයේ'), {'සංගමය+N+GenLoc+Sg+Inanim'})
+
+    def test_paksaya(self):
+        self.assertEqual(self._get_analyses('පක්ෂය'), {'පක්ෂය+N+Nom+Sg'})
+
+    def test_paksayee(self):
+        self.assertEqual(self._get_analyses('පක්ෂයේ'), {'පක්ෂය+N+GenLoc+Sg+Inanim'})
+
+    def test_paksayen(self):
+        self.assertEqual(self._get_analyses('පක්ෂයෙන්'), {'පක්ෂය+N+InstrAbl+Sg+Inanim'})
