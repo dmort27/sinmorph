0759656fbd67361d2e031a86af67353887b48fdc
David R. Mortensen <davidmortensen@gmail.com>
2018-07-09 09:48:00 -0400

    Extracted cons-final nouns

--- scripts/extract_nouns.py
+++ scripts/extract_nouns.py
@@ -7,8 +7,8 @@ import re
 def main(fn):
     with open(fn, encoding='utf-8') as f:
         for orth, phon, pos, gloss in csv.reader(f, dialect='excel-tab'):
-            if pos == 'NOUN' and re.search('aː$', phon):
-                print('{} LongANoun; ! {} "{}"'.format(orth[:-1], phon, gloss))
+            if pos == 'NOUN' and re.search('්$', orth):
+                print('{} ConsStem; ! {} "{}"'.format(orth[:-1], phon, gloss))
 
 if __name__ == '__main__':
     main(sys.argv[1])
